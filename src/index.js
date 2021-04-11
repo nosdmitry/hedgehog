0611ed8912bd154e306c82b07a77f5d7d524d634
Dmitry Nosov <mizai@yandex.ru>
2021-04-11 10:10:31 +0300

    Feature: order form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './index.scss';
+import $ from 'jquery';
+import 'slick-carousel';
+
+global.jQuery = $;
+global.$ = $;
 console.log('Hello!');
+
+// $(document).ready(function(){
+//   // $('.slick-carousel').slick({
+//   //   dots: false,
+//   //   infinite: true,
+//   //   arrows: true,
+//   //   speed: 300,
+//   //   slidesToShow: 1,
+//   //   centerMode: true,
+//   //   variableWidth: true,
+//   // });
+
+//   $('.slick-carousel').slick({
+//     dots: true,
+//     arrows: true,
+//     infinite: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 2
+//   });
+  
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -47,6 +48,10 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
   ]
 };
